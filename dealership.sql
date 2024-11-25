24325de8a6e4270fb0f1a8234bec926b99dbaeb9
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-11-25 09:59:22 -0500

    updated dealership with data

--- dealership.sql
+++ dealership.sql
@@ -53,3 +53,24 @@ CREATE TABLE `lease_contracts` (
   customer_name VARCHAR(50) NOT NULL,
   FOREIGN KEY (vin) REFERENCES vehicles(vin)
 );
+
+-- Insert some dealership data/info
+INSERT INTO dealership (name, address, phone) VALUES
+('City Motors', '123 Main St, Cityville', '555-1234'),
+('Metro Auto', '456 Metro Blvd, Metropolis', '555-5678');
+
+-- Insert some sample vehicle data
+INSERT INTO vehicles (vin, year, price, make, model, color, sold) VALUES
+('1A2B3C4D5E6F7G8H9I0J', 2021, 25000.00, 'Toyota', 'Camry', 'Red', FALSE),
+('2A3B4C5D6E7F8G9H0J1K', 2020, 22000.00, 'Honda', 'Civic', 'Blue', FALSE),
+('3A4B5C6D7E8F9G0H1J2K', 2022, 27000.00, 'Ford', 'Fusion', 'Black', FALSE),
+('4A5B6C7D8E9F0G1H2J3K', 2021, 32000.00, 'Chevrolet', 'Malibu', 'White', FALSE),
+('5A6B7C8D9E0F1G2H3J4K', 2023, 36000.00, 'BMW', 'X5', 'Silver', FALSE);
+
+-- Insert inventory data, associating vehicles with dealerships
+INSERT INTO inventory (dealership_id, vin) VALUES
+(1, '1A2B3C4D5E6F7G8H9I0J'),  -- Toyota Camry at City Motors
+(1, '2A3B4C5D6E7F8G9H0J1K'),  -- Honda Civic at City Motors
+(2, '3A4B5C6D7E8F9G0H1J2K'),  -- Ford Fusion at Metro Auto
+(2, '4A5B6C7D8E9F0G1H2J3K'),  -- Chevrolet Malibu at Metro Auto
+(1, '5A6B7C8D9E0F1G2H3J4K');  -- BMW X5 at City Motors
